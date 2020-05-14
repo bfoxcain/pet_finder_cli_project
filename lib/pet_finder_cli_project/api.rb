245a6a8ba858ae0c53b68d236ee3250b69470aca
bfoxcain <bfcain19@gmail.com>
2020-05-14 12:33:57 -0500

    Done.

---
+++ lib/pet_finder_cli_project/api.rb
@@ -0,0 +1,4 @@
+class Api
+end
+## service file/class responsible for talking to my api
+##going out to it for info and returning it.
---
+++ lib/pet_finder_cli_project/cli.rb
@@ -0,0 +1,5 @@
+class Cli
+
+end
+
+## handles input FROM user and OUTPUT to my user.
